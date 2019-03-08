9bf25698b3996fbc408d6336ec1da9a794cb71e0
Jacek Stępniewski <jaceks@gmail.com>
2019-03-08 19:25:09 +0100

    added ordering filter

--- metrics/filters.py
+++ metrics/filters.py
@@ -5,6 +5,7 @@ from .models import Metric
 
 GROUP_BY_FIELDS = ('date', 'channel', 'country', 'os')
 COUNTABLE_FIELDS = ('impressions', 'clicks', 'installs', 'spend', 'revenue')
+ALL_FIELDS = GROUP_BY_FIELDS + COUNTABLE_FIELDS
 
 
 class MetricFilter(filters.FilterSet):
@@ -12,6 +13,7 @@ class MetricFilter(filters.FilterSet):
     date_from = filters.DateFilter(field_name='date', lookup_expr='gte')
     group_by = filters.MultipleChoiceFilter(choices=zip(GROUP_BY_FIELDS, GROUP_BY_FIELDS),
                                             method='filter_group_by')
+    order_by = filters.OrderingFilter(fields=tuple(zip(ALL_FIELDS, ALL_FIELDS)))
 
     def filter_group_by(self, queryset, name, value):
         unused_fields = set(GROUP_BY_FIELDS) - set(value)
