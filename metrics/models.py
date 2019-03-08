476b895f755814c3c7beef15f4dac28c398bcd71
Jacek Stępniewski <jaceks@gmail.com>
2019-03-08 17:05:56 +0100

    added initial metric model

--- metrics/models.py
+++ metrics/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Metric(models.Model):
+    date = models.DateField()
+    channel = models.CharField(max_length=30)
+    country = models.CharField(max_length=2)
+    os = models.CharField(max_length=10)
+    impressions = models.IntegerField(default=0)
+    clicks = models.IntegerField(default=0)
+    installs = models.IntegerField(default=0)
+    spend = models.DecimalField(max_digits=8, decimal_places=2)
+    revenue = models.DecimalField(max_digits=8, decimal_places=2)
