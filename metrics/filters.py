da4c3e0d45dd0ee1c18387a8daca93b1999c7a19
Jacek Stępniewski <jaceks@gmail.com>
2019-03-08 19:09:29 +0100

    added group by filter

--- metrics/filters.py
+++ metrics/filters.py
@@ -1,11 +1,23 @@
+from django.db.models import Sum, F, CharField, Value
 from django_filters import rest_framework as filters
 
 from .models import Metric
+
+GROUP_BY_FIELDS = ('date', 'channel', 'country', 'os')
+COUNTABLE_FIELDS = ('impressions', 'clicks', 'installs', 'spend', 'revenue')
 
 
 class MetricFilter(filters.FilterSet):
     date_to = filters.DateFilter(field_name='date', lookup_expr='lte')
     date_from = filters.DateFilter(field_name='date', lookup_expr='gte')
+    group_by = filters.MultipleChoiceFilter(choices=zip(GROUP_BY_FIELDS, GROUP_BY_FIELDS),
+                                            method='filter_group_by')
+
+    def filter_group_by(self, queryset, name, value):
+        unused_fields = set(GROUP_BY_FIELDS) - set(value)
+        unused_annotation = {field_name: Value('---', CharField()) for field_name in unused_fields}
+        sums_annotation = {field_name: Sum(F(field_name)) for field_name in COUNTABLE_FIELDS}
+        return queryset.values(*value).annotate(**sums_annotation).annotate(**unused_annotation)
 
     class Meta:
         model = Metric
